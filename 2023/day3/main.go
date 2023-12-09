df06a9c28167e4e7b3e75efcef45103f557fbf1c
nkzk <nkzk95@gmail.com>
2023-12-09 04:32:46 +0100

    Continue day 3

--- 2023/day3/main.go
+++ 2023/day3/main.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
+
+func ReadSchematic(schematic [][]bool, line string) {
+	var number []int
+	for _, char := range line {
+		var x, y int
+		var newNumber int
+		for unicode.IsNumber(char) {
+			fmt.Printf("char: %s\n", string(char))
+		}
+
+		number = append(number, newNumber)
+
+	}
+	// for each line
+
+	// for each character
+	// if number, how many digits? whats the number
+	// position in line, x, y, z
+
+	// if line-1 og line+1 position x,y,z,x-1,z+1  != symbol = no number adjacent
+
+}
+
+func main() {
+	var schematic = [][]bool{}
+	input := "./input"
+	file, err := os.Open(input)
+	if err != nil {
+		fmt.Printf("Failed to open file")
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		ReadSchematic(schematic, scanner.Text())
+	}
+}
--- 2023/day3/main_test.go
+++ 2023/day3/main_test.go
@@ -1,8 +1,26 @@
 package main
 
-import "testing"
+import (
+	"bufio"
+	"os"
+	"testing"
+)
 
 func TestReadSchematic(t *testing.T) {
 
+	var schematic_test = [][]bool{}
+	input := "./test-input"
+	file, err := os.Open(input)
+	if err != nil {
+		t.Fatalf("failed to open file: %s", err)
+	}
+	defer file.Close()
 
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		ReadSchematic(schematic_test, scanner.Text())
+	}
+
+	// test schematic
 }
