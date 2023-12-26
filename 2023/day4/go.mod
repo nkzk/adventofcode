c4ce38cda190f09dc605ad470b6b53886c6ebec7
nkzk <nkzk95@gmail.com>
2023-12-26 04:16:21 +0100

    add day4, add make-day script

---
+++ 2023/day4/go.mod
@@ -0,0 +1,3 @@
+module github.com/nkzk/adventofcode/day4
+
+go 1.21.4
---
+++ 2023/day4/main_test.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+// Solves Part 1, returning points for each match/line. Sum is calculated in main/test function
+func PartOne(line string) int {
+	numbers := make(map[int]int)
+	cardPart := strings.Split(line, ":")
+	card := strings.Split(cardPart[1], "|")
+
+	// numbers on scratcher
+	for _, str := range strings.Fields(card[0]) {
+		num, err := strconv.Atoi(str)
+		if err != nil {
+			return 0
+		}
+		numbers[num] = num
+	}
+	sumMatch := 0
+	// check if numbers are a match.
+	for _, str := range strings.Fields(card[1]) {
+		num, err := strconv.Atoi(str)
+		if err != nil {
+			return 0
+		}
+		_, exists := numbers[num]
+		if exists {
+			if sumMatch == 0 {
+				sumMatch = 1
+			} else {
+				sumMatch += sumMatch
+			}
+		}
+	}
+	return sumMatch
+}
+
+func TestPartOne(t *testing.T) {
+
+	input := "./test-input"
+	file, err := os.Open(input)
+	if err != nil {
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var sum int
+	wantSum := 13
+	for scanner.Scan() {
+		sum += PartOne(scanner.Text())
+	}
+	if sum != wantSum {
+		t.Errorf("want %d got %d", wantSum, sum)
+	}
+}
+
+// Returns returns card id, number of matches
+func PartTwo(line string) (int, int) {
+	numbers := make(map[int]int)
+	cardPart := strings.Split(line, ":")
+	card := strings.Split(cardPart[1], "|")
+	idPart := strings.Split(cardPart[0], " ")
+	id, _ := strconv.Atoi(idPart[1])
+	// numbers on scratcher
+	for _, str := range strings.Fields(card[0]) {
+		num, err := strconv.Atoi(str)
+		if err != nil {
+			fmt.Printf("FAILED TO CONVERT STRING TO INT: %s", err)
+			return id, 0
+		}
+		numbers[num] = num
+	}
+	sumMatch := 0
+	// check if numbers are a match.
+	for _, str := range strings.Fields(card[1]) {
+		num, err := strconv.Atoi(str)
+		if err != nil {
+			fmt.Printf("FAILED TO CONVERT STRING TO INT: %s", err)
+			return id, 0
+		}
+		_, exists := numbers[num]
+		if exists {
+			if sumMatch == 0 {
+				sumMatch = 1
+			} else {
+				sumMatch++
+			}
+		}
+	}
+	return id, sumMatch
+}
+
+func TestPartTwo(t *testing.T) {
+
+	input := "./input"
+	file, err := os.Open(input)
+	if err != nil {
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var sum int
+	wantSum := 30
+	var cards []string
+	var numCards = make(map[int]int)
+	for scanner.Scan() {
+		cards = append(cards, scanner.Text())
+	}
+
+	// process originals
+	for _, card := range cards {
+		id, _ := PartTwo(card)
+		numCards[id] += 1 // Add 1 for the original card
+	}
+	// process copies
+	for _, card := range cards {
+		id, points := PartTwo(card)
+		for j := 1; j <= points; j++ {
+			next := id + j
+			if next <= len(cards) {
+				fmt.Printf("Adding 1 to Card %d\n", next)
+				numCards[next] += numCards[id]
+			}
+		}
+	}
+	sum = 0
+	for i := 1; i <= len(cards); i++ {
+		sum += numCards[i]
+		fmt.Printf("Card %d: %d (sum: %d)\n", i, numCards[i], sum)
+	}
+	if sum != wantSum {
+		t.Errorf("want %d got %d", wantSum, sum)
+	}
+
+}
