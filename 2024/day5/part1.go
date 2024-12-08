3f9a7740d5e6cf984086134983c3b78c23c7eb31
nkzk <nkzk95@gmail.com>
2024-12-08 01:07:17 +0100

    wip: day5

--- 2024/day5/part1.go
+++ 2024/day5/part1.go
@@ -1,6 +1,47 @@
 package main
 
+import (
+	"log"
+	"strconv"
+	"strings"
+)
+
 func Part1(input []string) int {
+	order := make(map[int]int)
+	updates := make([]string, 0)
+
+	for _, line := range input {
+		if strings.Contains(line, "|") {
+			parts := strings.Split(line, "|")
+			part1, err := strconv.Atoi(parts[0])
+			if err != nil {
+				log.Panicf("failed to convert string to int")
+			}
+
+			part2, err := strconv.Atoi(parts[1])
+			if err != nil {
+				log.Panic("failed to convert string to int")
+			}
+
+			order[part1] = part2
+		} else if strings.Contains(line, ",") {
+			updates = append(updates, line)
+		}
+	}
+
+	for _, update := range updates {
+		parts := strings.Split(update, ",")
+		for _, part := range parts {
+			number, err := strconv.Atoi(part)
+			if err != nil {
+				log.Panic("failed to convert string to int")
+			}
+			
+		}
+
+
+
+	}
 
 	return 0
 }
