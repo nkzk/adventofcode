575bd8e59f7055afbfe5306e0d53590f1d6f1663
nkzk <nkzk95@gmail.com>
2024-12-27 05:12:49 +0100

    feat: add day6 part 1

--- 2024/day6/part1.go
+++ 2024/day6/part1.go
@@ -1,38 +1,83 @@
 package main
 
+import (
+	"log/slog"
+	"utils/grid"
+)
+
 func Part1(input []string) int {
 	var sum int
 
-	type position struct {
-		x, y int
+	gridmap := grid.ArrayToGridMap(input)
+
+	visited := make(map[string]bool)
+
+	directionMap := map[string]string{
+		"^": "-1,0",
+		"<": "0,-1",
+		"v": "-1,0",
+		">": "0,1",
 	}
 
-	direction := map[string]position{
-		"up":    {-1, 0},
-		"down":  {1, 0},
-		"left":  {0, -1},
-		"right": {0, 1},
-	}
+	var direction string
+	var startingPosition string
 
-	startingPosition := position{}
+	var visitFn func(row, col int)
 
-	for i, line := range input {
-		for j, char := range line {
-			switch string(char) {
-			case ".":
-				continue
-			case "#":
-				continue
-			default:
-				startingPosition = position{i, j}
+	visitFn = func(row, col int) {
+		slog.Info("visiting", "x", row, "y", col)
+
+		if row < 0 || col < 0 || row >= len(input[0]) || col >= len(input) {
+			return
+		} else {
+			visited[grid.EncodeKey(row, col)] = true
+			x, y, _ := grid.DecodeKey(direction)
+
+			// set next location based on direction
+			dx := row + x
+			dy := col + y
+
+			// check if next is an obstruction
+			if gridmap[grid.EncodeKey(dx, dy)] == "#" {
+				slog.Info("next is obstruction")
+				// change direction 90 degrees
+				x, y, _ := grid.DecodeKey(direction)
+				direction = grid.EncodeKey(y, -x)
+
+				// overwrite next location based on new direction
+				dx = row + y
+				dy = col + -x
+
+				slog.Info("changed direction and visiting.",
+					"obstructionLocation", grid.EncodeKey(dx, dy),
+					"direction", direction)
 			}
+
+			visitFn(dx, dy)
+		}
+
+	}
+
+	for key, node := range gridmap {
+		switch node {
+		case ".":
+			continue
+		case "#":
+			continue
+		default:
+			direction = directionMap[node]
+			startingPosition = key
 		}
 	}
 
-	// 	case "^":
-	// case "<":
-	// case ">":
-	// case "v":
+	slog.Info("part 1", "starting position", startingPosition)
+
+	row, col, _ := grid.DecodeKey(startingPosition)
+
+	visitFn(row, col)
+	for range visited {
+		sum += 1
+	}
 
 	return sum
 }
