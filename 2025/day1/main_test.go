b34d14181a387454ac5d198bebb092a5ca9d5ecd
Nikita Z <54776184+nkzk@users.noreply.github.com>
2025-12-01 23:44:39 +0100

    chore: add 2025 day 1 (#2)

--- 2025/day1/main_test.go
+++ 2025/day1/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"testing"
 	"utils"
@@ -15,18 +16,97 @@ func Test(t *testing.T) {
 
 	t.Run("Part 1 ", func(t *testing.T) {
 		got := Part1(input)
-		want := 0
+		want := 3
 		if got != want {
 			t.Errorf("got %d want %d", got, want)
 		}
 	})
 
-    	t.Run("Part 2 ", func(t *testing.T) {
+	t.Run("Part 2 ", func(t *testing.T) {
 		got := Part2(input)
-		want := 0
+		want := 6
 		if got != want {
 			t.Errorf("got %d want %d", got, want)
 		}
+	})
+
+	t.Run("Part 2,2", func(t *testing.T) {
+		got := Part2([]string{"R150"})
+		want := 2
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("Part 2,3", func(t *testing.T) {
+		got := Part2([]string{"L150"})
+		want := 2
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("Part 2,4", func(t *testing.T) {
+		got := Part2([]string{"L50", "R1"})
+		want := 1
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("Part 2,8", func(t *testing.T) {
+		fmt.Printf("\nrunning part 2,5\n\n")
+		got := Part2([]string{"L50", "L50"})
+		want := 1
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+		fmt.Printf("\n\n")
+	})
+
+	t.Run("Part 2,5", func(t *testing.T) {
+		got := Part2([]string{
+			"L68",
+			"L30",
+			"R48",
+			"L5",
+			"R60",
+			"L55",
+			"L1",
+			"L99",
+			"R14",
+			"L82",
+			"R82", // 1
+			"R1",  // 0
+		})
+		want := 7
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+	t.Run("Part 2,6", func(t *testing.T) {
+		got := Part2([]string{"L650"})
+		want := 7
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+	t.Run("Part 2,7", func(t *testing.T) {
+		got := Part2([]string{"R50", "L50"})
+		want := 1
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("Part 2,8", func(t *testing.T) {
+		fmt.Printf("\nrunning 2,8\n\n")
+		got := Part2([]string{"R50", "R50"})
+		want := 1
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+		fmt.Printf("\n\n")
 	})
 
 }
--- 2025/day1/part1.go
+++ 2025/day1/part1.go
@@ -1,7 +1,49 @@
 package main
 
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
 func Part1(input []string) int {
 	var sum int
+	b := 50
+
+	for _, line := range input {
+		b = rotate(b, line)
+		if b == 0 {
+			sum += 1
+		}
+	}
 
 	return sum
+}
+
+func rotate(a int, rotation string) int {
+	direction := rotation[:1]
+	if direction != "L" && direction != "R" {
+		log.Printf("invalid direction, expected L or R but got: %s", direction)
+		os.Exit(1)
+	}
+
+	b, err := strconv.Atoi(rotation[1:])
+	if err != nil {
+		log.Printf("fatal error, check input! failed to convert %s to int: %v", rotation[1:], err)
+		os.Exit(2)
+	}
+
+	if direction == "L" {
+		return mod(a-b, 100)
+	}
+
+	return mod(a+b, 100)
+}
+
+func mod(a, m int) int {
+	a %= m
+	if a < 0 {
+		a += m
+	}
+	return a
 }
