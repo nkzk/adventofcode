72a66ee4f6c87af1095b4d7333800934eb7920be
nkzk <nkzk95@gmail.com>
2025-12-03 21:48:23 +0100

    chore: add 2025 day 3 part 1

--- 2025/day3/part1.go
+++ 2025/day3/part1.go
@@ -3,5 +3,31 @@ package main
 func Part1(input []string) int {
 	var sum int
 
+	for _, line := range input {
+		sum += largestJoltage(line)
+	}
+
 	return sum
+}
+
+func largestJoltage(s string) int {
+	largest := 0
+
+	for i, r := range s {
+		val := int(r - '0')
+		if i+1 < len(s) {
+			for _, v := range s[i+1:] {
+				val2 := int(v - '0')
+				if combine(val, val2) > largest {
+					largest = combine(val, val2)
+				}
+			}
+		}
+	}
+
+	return largest
+}
+
+func combine(a, b int) int {
+	return (a * 10) + b
 }
--- 2025/day3/part2.go
+++ 2025/day3/part2.go
@@ -3,23 +3,5 @@ package main
 func Part2(input []string) int {
 	var sum int
 
-	// for each bank
-	// for each digit
-
-	// vars:
-	// largestDigit := -1
-	// previousLargestDigit := -1
-
-	// sort!
-	// get last two
-
-	for _, line := range input {
-		digits := sort(line)
-	}
-
 	return sum
-}
-
-func sort(s string) []int {
-
 }
