aa1c7b307f56c3e4a8382d5846e2a45f9f0a2bc8
Nikita Zakharov <nikita.zakharov@statnett.no>
2025-12-03 17:09:00 +0100

    chore: initial day 3 commit

---
+++ 2025/day3/part2.go
@@ -0,0 +1,23 @@
+package main
+
+func Part2(input []string) int {
+	var sum int
+
+	// for each bank
+	// for each digit
+
+	// vars:
+    // largestDigit := -1
+	// previousLargestDigit := -1
+
+	// if this digit > largestDigit || last-digit out of index 
+	   // previousLargestDigit = largestDigit
+	   // largestDigit = this digit
+	
+	// 
+	for _, bank := range input {
+
+	}
+
+	return sum
+}
