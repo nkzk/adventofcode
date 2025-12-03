3364914149fe878ead2760ce36723d47e8a34570
nkzk <nkzk95@gmail.com>
2025-12-03 22:40:35 +0100

    chore: add 2025 day 3 part 2(wip)

--- 2025/day3/main_test.go
+++ 2025/day3/main_test.go
@@ -23,10 +23,19 @@ func Test(t *testing.T) {
 
 	t.Run("Part 2 ", func(t *testing.T) {
 		got := Part2(input)
-		want := 0
+		want := 3121910778619
 		if got != want {
 			t.Errorf("got %d want %d", got, want)
 		}
 	})
 
+}
+
+func TestCombine2(t *testing.T) {
+	have := []int{1, 2, 3, 4}
+	want := 1234
+	result := combine2(have...)
+	if result != want {
+		t.Errorf("want: %d, got %d", want, result)
+	}
 }
