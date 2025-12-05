0bd8955c056d2068fc8945e773845b309d753eab
nkzk <nkzk95@gmail.com>
2025-12-06 00:34:15 +0100

    test: add benchmark

--- 2025/day5/main_test.go
+++ 2025/day5/main_test.go
@@ -67,3 +67,60 @@ func Test(t *testing.T) {
 	})
 
 }
+
+func BenchmarkMerge(b *testing.B) {
+	var input []string
+	err := utils.ReadFile("input", &input)
+	if err != nil {
+		log.Panicf("failed to read file: %v", err)
+	}
+
+	var tests = []struct {
+		name  string
+		input []string
+	}{
+		{
+			name:  "full",
+			input: input,
+		},
+		{
+			name: "test",
+			input: []string{
+				"200-300",
+				"100-101",
+				"1-1",
+				"2-2",
+				"3-3",
+				"1-3",
+				"1-3",
+				"2-2",
+				"50-70",
+				"10-10",
+				"98-99",
+				"99-99",
+				"99-99",
+				"99-100",
+				"1-1",
+				"2-1",
+				"100-100",
+				"100-100",
+				"100-101",
+				"200-300",
+				"201-300",
+				"202-300",
+				"250-251",
+				"98-99",
+				"100-100",
+				"100-101",
+				"1-101",
+			},
+		},
+	}
+
+	b.ResetTimer()
+	for _, t := range tests {
+		b.Run(t.name, func(b *testing.B) {
+			Part2(t.input)
+		})
+	}
+}
